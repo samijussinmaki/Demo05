2d6db8552eae5405e2675fadd861febb18856852
K2469 <K2469@student.jamk.fi>
2017-02-09 09:40:24 +0200

    tuntiharjoituksia

--- tuntiharjoituksia5/Program.cs
+++ tuntiharjoituksia5/Program.cs
@@ -10,6 +10,41 @@ namespace tuntiharjoituksia5
     {
         static void Main(string[] args)
         {
+            // create a list for integers
+            List<int> list = new List<int>();
+            // boolean variable, just check for looping
+            bool result;
+            // ask numbers
+            do
+            {
+                // ask number from the user (read one line)
+                Console.Write("Give a number > ");
+                string line = Console.ReadLine();
+                // try to read number from the given line
+                int number;
+                result = int.TryParse(line, out number);
+                // number (integer) was given correctly, use it..
+                if (result)
+                {
+                    list.Add(number);
+                }
+            } while (result);
+            // write some statistics
+            Console.WriteLine("Average : {0}", list.Average());
+            Console.WriteLine("Min : {0}", list.Min());
+            Console.WriteLine("Max : {0}", list.Max());
+            Console.WriteLine("Numbers count : {0}", list.Count);
+            // all
+            list.ForEach(item => Console.Write(item + ","));
+            // sort
+            list.Sort();
+            // all
+            Console.WriteLine();
+            list.ForEach(item => Console.Write(item + ","));
+            // read line to keep writelines visible
+            Console.ReadLine();
+
+
         }
     }
 }
